060b29f92ad1ea3946387940549bd91776fe87e4
Nikita Tret <accauntnikity@gmail.com>
2019-02-11 02:03:07 +0300

    Implemntation

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,8 +6,13 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int temp = 0;
+            for (int i = 0; i < count; i++)
+            {
+                temp = temp + number + add * i;
+            }
+            return temp;
+            
         }
     }
 }
